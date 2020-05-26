7ae0e47ee9a40d6b32c4231985cd7fa812f0f9c5
Maks Briliant <47421740+mxup@users.noreply.github.com>
2020-05-26 18:40:50 +0300

    Add files via upload

---
+++ scripts/reverse
@@ -0,0 +1,4 @@
+#!/usr/bin/ruby
+
+array = [1000, 999, 956, 96, 12, 8, 6, 4]
+puts array.reverse()
---
+++ scripts/youtube.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xset -dpms
+xset s off
+xset s noblank
+#--start-fullscreen	
+
+google-chrome-beta  --noerrdialogs --fast-start --app="https://youtu.be/KPa1_7AF1lM" --window-size=352,260
+##https://www.youtube.com/watch?v=DzJasz19EnA
